b274d90ae55b9a17ad95ca08e18d9eb4c6fcc04e
Your Name <you@example.com>
2024-03-11 06:12:45 -0300

    primeiro comando no banco

--- app.py
+++ app.py
@@ -7,8 +7,15 @@ conexao= mysql.connector.connect(
   password = "Acesso@123",
   database = "dbboessoal"
 )
-
 cursor = conexao.cursor()
+
+comando = 'INSERT into vendas (nome_produto, valor)'#comando
+
+cursor.execute(comando)#executa comando
+
+conexao.commit()#edita o banco de dados
+#resultado = cursor.fetchall()#ler o banco de dados
+
 
 cursor.close()
 conexao.close()
