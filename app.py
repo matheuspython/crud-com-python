adf8d0a5c86658d121c1d6c42387231ddd4710ce
Your Name <you@example.com>
2024-03-11 06:47:11 -0300

    refatorando codigo de maneira funcional

--- app.py
+++ app.py
@@ -8,44 +8,30 @@ conexao= mysql.connector.connect(
   database = "dbboessoal"
 )
 cursor = conexao.cursor()
-#create data 
-nome_produto = "toddynho"
-valor = 3
 
-comando = f'INSERT into vendas (nome_produto, valor) VALUES ("{nome_produto}", "{valor}")'#comando
+def create(name, value):
+  comando = f'INSERT into vendas (nome_produto, valor) VALUES ("{nome}", "{value}")'#
+  cursor.execute(comando)#executa comando
+  readDatabase()
 
-cursor.execute(comando)#executa comando
+def readDatabase():
+  comando = f'SELECT * FROM vendas'
+  cursor.execute(comando)
+  resultado = cursor.fetchall()
+  print(resultado)
 
-# conexao.commit()#edita o banco de dados
-#resultado = cursor.fetchall()#ler o banco de dados
-
-#read data
-comando = f"SELECT * FROM vendas"
-cursor.execute(comando)
-resultado = cursor.fetchall()
-print(resultado)
-
-
-
-#update
-# nome_produto = "toddynho"
-# valor = 6
-# comando = f'UPDATE vendas SET valor = {valor} WHERE nome_produto = "{nome_produto}"'
-# cursor.execute(comando)
-# conexao.commit()#edita o banco de dados
-
-#delete
-# nome_produto = "toddynho"
-# valor = 6
-# comando = f'DELETE FROM vendas where nome_produto = "{nome_produto}"'
-# cursor.execute(comando)
-# conexao.commit()
-
-
-
-
-
-
-
+def updateDatabase(name, value):
+  comando = f'UPDATE vendas SET valor = {value} WHERE nome_produto = {name}'
+  cursor.execute(comando)
+  conexao.commit()
+  readDatabase()
+ 
+def delete(name, value):
+  comando - f'DELETE FROM vendas where nome_produto = {name}'
+  cursor.execute(comando)
+  conexao.commit()
+  readDatabase()
+  
+  
 cursor.close()
 conexao.close()
