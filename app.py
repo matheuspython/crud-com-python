f56e1ece99fb2ca812301d42e6399f8f826910ee
Your Name <you@example.com>
2024-03-11 06:36:32 -0300

    funcionalidade de delete adicionada

--- app.py
+++ app.py
@@ -9,28 +9,41 @@ conexao= mysql.connector.connect(
 )
 cursor = conexao.cursor()
 #create data 
-# nome_produto = "toddynho"
-# valor = 3
+nome_produto = "toddynho"
+valor = 3
 
-# comando = f'INSERT into vendas (nome_produto, valor) VALUES ("{nome_produto}", "{valor}")'#comando
+comando = f'INSERT into vendas (nome_produto, valor) VALUES ("{nome_produto}", "{valor}")'#comando
 
-# cursor.execute(comando)#executa comando
+cursor.execute(comando)#executa comando
 
 # conexao.commit()#edita o banco de dados
 #resultado = cursor.fetchall()#ler o banco de dados
 
 #read data
-# comando = f"SELECT * FROM vendas"
-# cursor.execute(comando)
-# resultado = cursor.fetchall()
-# print(resultado)
+comando = f"SELECT * FROM vendas"
+cursor.execute(comando)
+resultado = cursor.fetchall()
+print(resultado)
 
 
 
 #update
-nome_produto = "toddynho"
-valor = 6
-comando = f'UPDATE vendas SET valor = {valor} WHERE nome_produto = "{nome_produto}"'
+# nome_produto = "toddynho"
+# valor = 6
+# comando = f'UPDATE vendas SET valor = {valor} WHERE nome_produto = "{nome_produto}"'
+# cursor.execute(comando)
+# conexao.commit()#edita o banco de dados
+
+#delete
+# nome_produto = "toddynho"
+# valor = 6
+# comando = f'DELETE FROM vendas where nome_produto = "{nome_produto}"'
+# cursor.execute(comando)
+# conexao.commit()
+
+
+
+
 
 
 
