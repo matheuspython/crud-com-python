929a748ee19701205eb427e00685baf50b4a6363
Your Name <you@example.com>
2024-03-11 07:06:10 -0300

    adicionando perguntas

--- app.py
+++ app.py
@@ -36,5 +36,41 @@ def delete(name):
   
    
 
+
+
+
+def main():
+    while True:
+        print("1. Criar um novo produto")
+        print("2. Atualizar o valor de um produto")
+        print("3. Deletar um produto")
+        print("4. Visualizar todos os produtos")
+        print("5. Sair")
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            name = input("Digite o nome do produto: ")
+            value = input("Digite o valor do produto: ")
+            create(name, value)
+        elif opcao == "2":
+            name = input("Digite o nome do produto que deseja atualizar: ")
+            value = input("Digite o novo valor do produto: ")
+            updateDatabase(name, value)
+        elif opcao == "3":
+            name = input("Digite o nome do produto que deseja deletar: ")
+            delete(name)
+        elif opcao == "4":
+            readDatabase()
+        elif opcao == "5":
+            break
+        else:
+            print("Opção inválida. Por favor, escolha uma opção válida.")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
 cursor.close()
 conexao.close()
